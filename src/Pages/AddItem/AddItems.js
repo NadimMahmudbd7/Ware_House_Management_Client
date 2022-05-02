14f5ebff1987a13a1f87b4fe82c943f6a30aea80
Nadim Mahmud <giftsframebd@gmail.com>
2022-05-02 20:26:07 +0600

    error handle

--- src/Pages/AddItem/AddItems.js
+++ src/Pages/AddItem/AddItems.js
@@ -7,27 +7,23 @@ import auth from '../../firebase.init';
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
     const [user] = useAuthState(auth)
-    console.log(user);
     const onSubmit = data => {
-        console.log(data);
         fetch('http://localhost:4000/products', {
             method: "POST",
             headers: {
-                "authorization":`${user.email} ${localStorage.getItem("AccessToken")}`,
+                "authorization":`${user?.email} ${localStorage.getItem("AccessToken")}`,
                 "content-type": "application/json"
             },
             body: JSON.stringify(data)
         })
             .then(res => res.json())
-            .then(result => {
-                console.log(result);
-            })
+            .then(result => console.log(result))
     }
     return (
         <div>
             <h1 className='text-center my-4'>Add Your Item:</h1>
             <form className='inputForm login-container' onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("email")} placeholder='noraml input' value={user?.email} readOnly disabled id="" />
+                <input {...register("email")} placeholder='noraml input' value={user?.email} readOnly id="" />
                 <input {...register("name", { required: true, maxLength: 20 })} placeholder="Problem Name" required />
                 <textarea className='my-3 rounded' {...register("description")} id="" cols="10" rows="4" placeholder='Solving Description' required ></textarea>
                 <input type="number" {...register("price")} />
